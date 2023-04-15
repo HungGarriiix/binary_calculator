4680a0a72921b64cc246df6fb67a2b67da2515a6
HungGarriiix <56022784+HungGarriiix@users.noreply.github.com>
2023-04-15 13:41:36 +0700

    Added independant Discord bot API to the project

---
+++ binaries/discord_bot_api/Bot.cs
@@ -0,0 +1,55 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discord_bot_api
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = true,
+                EnableMentionPrefix = true
+            };
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient a, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
