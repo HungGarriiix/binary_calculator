e84edbaa854bdff7d6d374e4a923e787a85e31dd
HungGarriiix <56022784+HungGarriiix@users.noreply.github.com>
2023-04-19 19:59:33 +0700

    Added command examples

--- binaries/discord_bot_api/Bot.cs
+++ binaries/discord_bot_api/Bot.cs
@@ -1,19 +1,24 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
 using Newtonsoft.Json;
+using discord_bot_api.Commands;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace discord_bot_api
 {
     public class Bot
     {
         public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
 
         public async Task RunAsync()
         {
@@ -24,23 +29,31 @@ namespace discord_bot_api
 
             var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
 
-            var config = new DiscordConfiguration
+            var config = new DiscordConfiguration()
             {
                 Token = configJson.Token,
                 TokenType = TokenType.Bot,
-                AutoReconnect = true
+                Intents = DiscordIntents.All,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
             };
 
             Client = new DiscordClient(config);
+            
 
             Client.Ready += OnClientReady;
 
-            var commandsConfig = new CommandsNextConfiguration
+            var commandsConfig = new CommandsNextConfiguration()
             {
                 StringPrefixes = new string[] { configJson.Prefix },
-                EnableDms = true,
-                EnableMentionPrefix = true
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                EnableDefaultHelp = true,
+                DmHelp = true,
             };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+            Commands.RegisterCommands<CalCommands>();
 
             await Client.ConnectAsync();
 
